864fb6362c57801899a59f65a1282c4fb544287b
Daphne Simons <daphnejasminesimons@gmail.com>
2025-03-18 11:51:02 +1300

    comments in code to help with creating a cart state

--- client/components/ProductsList.tsx
+++ client/components/ProductsList.tsx
@@ -5,12 +5,15 @@ import ProductsByCategory from './ProductsByCategory'
 import { Product } from '../../models/product'
 
 function ProductsList() {
+  // TODO: this is the 'local' cart state.
   const [cart, setCart] = useState<{ product: Product; quantity: number }[]>([])
   console.log('first load cart', cart)
 
   const filterProductsByCategory = (categoryId: number) => {
     return products.filter((product) => product.category_id === categoryId)
   }
+  // TODO: this addToCart will call a mutation on an existing cartData query, with react query
+  // (you will need to define the query and the mutation already, ideally in the hooks folder)
   const addToCart = (product: Product, quantity: number) => {
     setCart([...cart, { product, quantity }])
     console.log(`Added ${quantity} of ${product.name} to cart.`)
---
+++ client/hooks/useCart.ts
@@ -0,0 +1,13 @@
+// TODO create some custom hooks that do a couple of things.
+
+// 1. useQuery to get cartData
+// NOTE: will need to make a backend database for cartData and connect to it.
+// In order to create a unique cartData record in the database, your database query for "AddToCart", will need to return the id for you to use on the client side.
+
+// 2. useMutation to add new items and quanitites to the cartData record by cartId
+
+// 4. useMutation to update / edit the cartData record by cartId
+
+// 3. useMutation to remove items from the cartData record by cartId
+
+// STRETCH: figure out a way to hold your "cartData" in lcoal storage, or a session, and only when someone makes a purchase, does it add an "order" record to the database. This way you're not clogging up your database with half completed orders.

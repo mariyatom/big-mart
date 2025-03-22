2eee3faf22c7ec3c3c02a005325cc5fa70492a11
mariyatom <mariyatom07@gmail.com>
2025-03-22 19:30:35 +1300

    get api for all categories

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,11 @@
 import connection from './connection.ts'
 import { Fruit } from '../../models/fruit.ts'
+import { Category } from '../../models/category.ts'
 
 export async function getAllFruits(db = connection): Promise<Fruit[]> {
   return db('fruit').select()
+}
+
+export async function getAllCategories(): Promise<Category[]> {
+  return await connection('category').select('*')
 }
